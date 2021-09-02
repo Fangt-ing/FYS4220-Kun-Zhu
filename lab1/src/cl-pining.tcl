64568e1db1162507d3945c7c5e5c97b7875762b6
Kun Zhu <kunzh@ulrik.uio.no>
2021-09-02 18:11:26 +0200

    Seven segment display completed, but if statement not implemented.

--- lab1/src/cl-pining.tcl
+++ lab1/src/cl-pining.tcl
@@ -1,17 +1,29 @@
-# input pin setup for slide swtiches
+#Toggle switches
 set_location_assignment PIN_C10 -to sw[0]
 set_location_assignment PIN_C11 -to sw[1]
 set_location_assignment PIN_D12 -to sw[2]
 set_location_assignment PIN_C12 -to sw[3]
 
-# output pin setup for led
+#Enable the counter
+# set_location_assignment PIN_f15 -to tocount
+
+#LED outputs
 set_location_assignment PIN_A8 -to led[0]
 set_location_assignment PIN_A9 -to led[1]
 set_location_assignment PIN_A10 -to led[2]
 set_location_assignment PIN_B10 -to led[3]
 
-# output pin setup for 7-segment
-set_location_assignment PIN_c14 -to seven[0]
-set_location_assignment PIN_e15 -to seven[1]
-set_location_assignment PIN_c15 -to seven[2]
-set_location_assignment PIN_c16 -to seven[3]
+# set_location_assignment PIN_B11 -to tocountled
+
+# 7-segment
+set_location_assignment PIN_c14 -to sevenled[0]
+set_location_assignment PIN_e15 -to sevenled[1]
+set_location_assignment PIN_c15 -to sevenled[2]
+set_location_assignment PIN_c16 -to sevenled[3]
+set_location_assignment PIN_e16 -to sevenled[4]
+set_location_assignment PIN_d17 -to sevenled[5]
+set_location_assignment PIN_c17 -to sevenled[6]
+set_location_assignment PIN_d15 -to sevenled[7]
+
+#To avoid that the FPGA is driving an unintended value on pins that are not in use:
+set_global_assignment -name RESERVE_ALL_UNUSED_PINS "AS INPUT TRI-STATED"
