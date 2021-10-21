86c1a7c5c97abb297c179b82456aa508acb09050
Kun Zhu <kunzh@ulrik.uio.no>
2021-10-21 12:58:24 +0200

    renamed to nio2_system

--- MCU/hdl/system_top.vhd
+++ MCU/hdl/system_top.vhd
@@ -1,28 +1,27 @@
 library ieee;
-context ieee.ieee_std_context;
-use ieee.math_real.all;
+use ieee.std_logic_1164.all;
+
 entity system_top is
     port (
         clk    : in std_logic;
-        arst_n : in std_logic;
-
+        arst_n : in std_logic
     );
 end entity;
 
 architecture rtl of system_top is
-    component nios2sys is
+
+    component nios2_system is
         port (
             clk_clk       : in std_logic := 'X'; -- clk
             reset_reset_n : in std_logic := 'X'  -- reset_n
         );
-    end component nios2sys;
+    end component nios2_system;
 
 begin
 
-    u0 : component nios2sys
+    u0 : component nios2_system
         port map(
             clk_clk       => clk,   --   clk.clk
             reset_reset_n => arst_n -- reset.reset_n
         );
-
     end architecture;
