2f131d98c32c8db62e89c4d48cf9cb278e65258b
kunzh <kunzh@uio.no>
2021-11-04 17:54:11 +0100

    17.2 completed, with sth to be fixed.

--- MCU/software/app_rtos/app_rtos.c
+++ MCU/software/app_rtos/app_rtos.c
@@ -1,71 +1,146 @@
+#include <stdio.h>
 #include "includes.h"
-#include <stdio.h> // encounter error message "incompatible implicit declaration of built-in function 'printf'"
-#include <string.h> // the strlen function requires the string.h
+#include <string.h>
 
-/* Definition of Task stacks */
-#define TASK_STACKSIZE 2048  // Number of 32 bit words (e.g. 8192 bytes)
+#include "altera_avalon_pio_regs.h" //access to PIO macros
+#include <sys/alt_irq.h>            // access to the IRQ routines
+
+#define TASK_STACKSIZE 1024 // Number of 32 bit words (e.g. 8192 bytes)
 OS_STK task1_stk[TASK_STACKSIZE];
 OS_STK task2_stk[TASK_STACKSIZE];
 
 #define TASK1_PRIORITY 4
 #define TASK2_PRIORITY 5
 
+//Semaphore to protect the shared JTAG resource
+OS_EVENT *shared_jtag_sem;
+// synchronization semaphore
+OS_EVENT *shared_key1_sem;
 
-void task1(void* pdata)
+// global variable to hold the value of the edge capture register.
+volatile int edge_capture;
+
+static void handle_interrupts(void *context)
 {
-    char text[] = "###task1\n";
+    // Cast context to edge_capture's type
+    // Volatile to avoid compiler optimization
+    // this will point to the edge_capture variable.
+    volatile int *edge_capture_ptr = (volatile int *)context;
+    //Read the edge capture register on the PIO and store the value
+    //The value will be stored in the edge_capture variable and accessible
+    //from other parts of the code.
+    *edge_capture_ptr = IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_IRQ_BASE);
+
+    //Write to edge capture register to reset it
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_IRQ_BASE, 0);
+    // // Read the edge capture register on the PIO and store the value
+    // // The value will be stored in the edge_capture variable and accessible
+    // // from other parts of the code.
+    // *uart_status_ptr = IORD(UART_STATUS_BASE, 2);
+    // // Write to edge capture register to reset it
+    // IOWR(UART_STATUS_BASE, 2, 0);
+    OSSemPost(shared_key1_sem);
+}
+
+/* This function is used to initializes and registers the interrupt handler. */
+static void init_interrupt_pio()
+{
+    // Recast the edge_capture point to match the
+    // alt_irq_register() function prototypo
+    void *edge_capture_ptr = (void *)&edge_capture;
+
+    // Enable a single interrupt input by writing a one to the corresponding interruptmask bit locations
+    IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_IRQ_BASE, 0x1);
+
+    // Reset the edge capture register
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_IRQ_BASE, 0);
+
+    /* In order to keep the impact of interrupts on the execution of the main program to a minimum,
+    it is important to keep interrupt routines short. If additional processing is necessary for a
+    particular interrupt, it is better to do this outside of the ISR. E.g., checking the value
+    of the edge_capture variable.*/
+
+    alt_ic_isr_register(PIO_IRQ_IRQ_INTERRUPT_CONTROLLER_ID,
+                        PIO_IRQ_IRQ, handle_interrupts, edge_capture_ptr, 0x0);
+}
+
+void task1(void *pdata)
+{
+    char text[] = "Hello from Task1\n";
     int i;
-    while(1)
+    alt_u8 error_code = OS_NO_ERR;
+    while (1)
     {
-        for (i = 0; i < strlen(text); i++){
-        putchar(text[i]);
+        // Collect semaphore before writing to JTAG UART
+        // wait for semaphore from interrupt handling routine before executing the taks
+        OSSemPend(shared_key1_sem, 0, &error_code);
+
+        // Collect semaphore before writing to JTAG UART
+        OSSemPend(shared_jtag_sem, 0, &error_code);
+        for (i = 0; i < strlen(text); i++)
+        {
+            putchar(text[i]);
         }
-        // printf("Hello from task1\n");
-        OSTimeDlyHMSM(0, 0, 3, 0); // (hours, minutes, seconds, milliseconds)
+        // Release semaphore
+        OSSemPost(shared_jtag_sem);
+        OSSemPost(shared_key1_sem);
+        //printf("Hello from task2\n");
+        OSTimeDlyHMSM(0, 0, 1, 20);
     }
 }
 
-void task2(void* pdata)
+void task2(void *pdata)
 {
-    char text[] = "!!!!@@@task2\n";
+    char text[] = "Thomas speaking!\n";
     int i;
-    while(1)
+    alt_u8 error_code = OS_NO_ERR;
+    while (1)
     {
-        for ( i = 0; i < strlen(text); i++){
+        // Collect semaphore before writing to JTAG UART
+        OSSemPend(shared_jtag_sem, 0, &error_code);
+        for (i = 0; i < strlen(text); i++)
+        {
             putchar(text[i]);
         }
-        // printf("@@@This is task2@@@\n");
-        OSTimeDlyHMSM(0, 0, 3, 0); // (hours, minutes, seconds, milliseconds)
+        // Release semaphore
+        OSSemPost(shared_jtag_sem);
+        //printf("Hello from task2\n");
+        OSTimeDlyHMSM(0, 0, 1, 4);
     }
 }
 
 /* The main function creates two task and starts multi-tasking */
 int main(void)
-{   
-    //Create the task
-    OSTaskCreateExt(task1, //Pointer to task function
-                NULL, // pointer to argument that is passed to task
-                (void *)&task1_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
-                TASK1_PRIORITY, // Task priority
-                TASK1_PRIORITY, // Task ID - same as priority
-                task1_stk, // Pointer to bottom of task stack
-                TASK_STACKSIZE, // Stacksize
-                NULL, // Pointer to user supplied memory
-                0); // Various task options
+{
 
-    //Create the task
-    OSTaskCreateExt(task2, //Pointer to task function
-                NULL, // pointer to argument that is passed to task
-                (void *)&task2_stk[TASK_STACKSIZE-1], // Pointer to top of task stack
-                TASK2_PRIORITY, // Task priority
-                TASK2_PRIORITY, // Task ID - same as priority
-                task2_stk, // Pointer to bottom of task stack
-                TASK_STACKSIZE, // Stacksize
-                NULL, // Pointer to user supplied memory
-                0); // Various task options
+    //create JTAG semaphore and initialize to 1
+    shared_jtag_sem = OSSemCreate(1);
+    // create synchronization semaphore and initialize to 0
+    shared_key1_sem = OSSemCreate(0);
 
-    //Start the multi-tasking system.
+    // initialize interrupt
+    init_interrupt_pio();
+
+    OSTaskCreateExt(task1,
+                    NULL,
+                    (void *)&task1_stk[TASK_STACKSIZE - 1],
+                    TASK1_PRIORITY,
+                    TASK1_PRIORITY,
+                    task1_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
+
+    OSTaskCreateExt(task2,
+                    NULL,
+                    (void *)&task2_stk[TASK_STACKSIZE - 1],
+                    TASK2_PRIORITY,
+                    TASK2_PRIORITY,
+                    task2_stk,
+                    TASK_STACKSIZE,
+                    NULL,
+                    0);
+
     OSStart();
-    return 0; 
+    return 0;
 }
-
--- MCU/software/app_rtos/obj/default/app_rtos.d
+++ MCU/software/app_rtos/obj/default/app_rtos.d
@@ -6,7 +6,10 @@ obj/default/./app_rtos.o: app_rtos.c ../app_rtos_bsp/HAL/inc/includes.h \
  ../app_rtos_bsp/HAL/inc/sys/alt_alarm.h \
  ../app_rtos_bsp/HAL/inc/sys/alt_llist.h \
  ../app_rtos_bsp/HAL/inc/priv/alt_alarm.h \
- ../app_rtos_bsp/UCOSII/inc/ucos_ii.h ../app_rtos_bsp/HAL/inc/os_cpu.h
+ ../app_rtos_bsp/UCOSII/inc/ucos_ii.h ../app_rtos_bsp/HAL/inc/os_cpu.h \
+ ../app_rtos_bsp/drivers/inc/altera_avalon_pio_regs.h \
+ ../app_rtos_bsp/HAL/inc/io.h ../app_rtos_bsp/HAL/inc/alt_types.h \
+ ../app_rtos_bsp/HAL/inc/sys/alt_irq.h
 
 ../app_rtos_bsp/HAL/inc/includes.h:
 
@@ -33,3 +36,11 @@ obj/default/./app_rtos.o: app_rtos.c ../app_rtos_bsp/HAL/inc/includes.h \
 ../app_rtos_bsp/UCOSII/inc/ucos_ii.h:
 
 ../app_rtos_bsp/HAL/inc/os_cpu.h:
+
+../app_rtos_bsp/drivers/inc/altera_avalon_pio_regs.h:
+
+../app_rtos_bsp/HAL/inc/io.h:
+
+../app_rtos_bsp/HAL/inc/alt_types.h:
+
+../app_rtos_bsp/HAL/inc/sys/alt_irq.h:
